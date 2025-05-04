ba738e7efda4992323dc508f3b01ea792571b136
NIOX <notniox.gonzales@gmail.com>
2025-05-04 15:04:24 +0800

    Corrected error caused by misnamed variable

--- src/components/CSVGraph.js
+++ src/components/CSVGraph.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Papa from 'papaparse';
 import {
-    LineChart, Line, XAxis, YAxis,
-    CartesianGrid, Tooltip, Legend
+    LineChart, Line, XAxis, YAxis, Bar,
+    CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 
 const CSVGraph = () => {
